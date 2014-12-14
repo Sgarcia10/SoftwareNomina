cf7b0708a6b0311851c73acc5d636c32f586ba42
Sgarcia10 <garciasantiago10@gmail.com>
2014-12-13 22:59:38 -0500

    Inicio

--- src/empresa/mundo/SoftwareNomina.java
+++ src/empresa/mundo/SoftwareNomina.java
@@ -11,11 +11,6 @@ package empresa.mundo;
  */
 public class SoftwareNomina {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+   
     
 }
