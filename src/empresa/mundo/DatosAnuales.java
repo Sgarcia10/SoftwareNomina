2ba587c576a6362b0e2eda19a77df3c08d9dbc23
Sgarcia10 <garciasantiago10@gmail.com>
2014-12-19 13:45:40 -0500

    Inicio

---
+++ src/empresa/mundo/DatosAnuales.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empresa.mundo;
+
+/**
+ *
+ * @author Santiago
+ */
+public class DatosAnuales 
+{
+    private int año = 2015;
+    
+    private int salarioMinimoMensual = 616000;
+    
+    private int auxilioTransporte = 72000;
+    
+    
+}
--- src/empresa/mundo/Empleado.java
+++ src/empresa/mundo/Empleado.java
@@ -19,6 +19,8 @@ public class Empleado
     
     private String cargo;
     
+    private int salarioBasico;
+    
     
     public Empleado(String nombresP, String apellidosP, int cedulaP)
     {
